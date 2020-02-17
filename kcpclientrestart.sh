aa46d145c567e1e9ba6a5a95d61bbbb5605c3c07
jfplyl999 <50453655+jfplyl999@users.noreply.github.com>
2020-02-17 12:28:05 +0800

    kcp客户端重启

--- kcpclientrestart.sh
+++ kcpclientrestart.sh
@@ -2,6 +2,7 @@
     ps -ef|grep kcptun|grep -v grep|cut -c 9-15|xargs kill -9
     ps -ef|grep udp2raw|grep -v grep|cut -c 9-15|xargs kill -9
     sleep 3
+    cd /root
     nohup ./run.sh ./udp2raw_amd64 -c -r 255.255.255.255:8088 -l0.0.0.0:9090 --raw-mode faketcp --cipher-mode none -a -k "atrandys" >udp2raw.log 2>&1 &
     nohup ./run.sh ./client_linux_amd64 -c ./kcptun_client.json >kcptun.log 2>&1 &
     sleep 3
